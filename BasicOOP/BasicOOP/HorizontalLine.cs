6faedc8981385b66be6959264f8679a99906023c
Azam003 <azam00399@gmail.com>
2017-06-29 20:21:14 +0300

    Horizontal Line

---
+++ BasicOOP/BasicOOP/HorizontalLine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicOOP
+{
+	public class HorizontalLine
+	{
+		List<Point> pList = new List<Point>();
+
+		public HorizontalLine(int xLeft, int xRight, int y, char sym) 
+		{
+			for (int x = xLeft; x < xRight; x++)
+			{
+				Point p = new Point(x, y, sym);
+				pList.Add(p);
+			}
+		}
+
+		public void Draw() 
+		{
+			foreach (Point p in pList)
+			{
+				p.Draw();
+			}
+		}
+	}
+}
--- BasicOOP/BasicOOP/Program.cs
+++ BasicOOP/BasicOOP/Program.cs
@@ -11,39 +11,13 @@ namespace BasicOOP
 			p1.Draw();
 
 
-			Point p2 = new Point(4, 5, '#');
+			Point p2 = new Point(2, 6, '#');
 			p2.Draw();
 
-			// List
-			List<int> numList = new List<int>();
-            numList.Add ( 0 );
-            numList.Add ( 1 );
-            numList.Add ( 2 );
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-			foreach (int i in numList)
-			{
-				Console.WriteLine(i);
-			}
-
-			numList.RemoveAt(0);
-			Console.WriteLine();
-
-			foreach (int i in numList)
-			{
-				Console.WriteLine(i);
-			}
-
-			Console.WriteLine();
-
-			List<Point> pList = new List<Point>();
-			pList.Add( p1 );
-			pList.Add( p2 );
-			Console.WriteLine("p1.x - " + p1.x);
-			Console.WriteLine("p2.x - " +p2.x);
+			HorizontalLine hLine = new HorizontalLine(5, 20, 8, '*');
+			HorizontalLine hLineDown = new HorizontalLine(5, 20, 16, '*');
+			hLine.Draw();
+			hLineDown.Draw();
 		}
 	}
 }
