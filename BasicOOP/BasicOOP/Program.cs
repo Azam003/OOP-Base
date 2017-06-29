5a9d54782d018222b3829a41a4a01cce69e5f9ed
Azam003 <azam00399@gmail.com>
2017-06-29 15:46:37 +0300

    New Script

---
+++ BasicOOP/BasicOOP/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BasicOOP
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.ReadKey ();
+		}
+	}
+}
